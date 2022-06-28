6f507c87328d11d7b5b5fcd105a9a30c923b756e
Bayyu5 <100985225+Bayyu5@users.noreply.github.com>
2022-06-28 10:45:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ global.owner = [
   ['+84 82 430 4616'],
   ['+84 34 979 0091'],
   ['+62 818-0545-0249'],
-  ['17608914335', 'Papah-Chan', true],
-  ['+84 82 430 4616', 'Bot', true],
+  ['+62 878-5907-1938', 'Bot', true],
+  ['+62 821-3631-2583', 'VAS BAYU', true],
   ['+84 34 979 0091', 'Johannes', true],
   ['+62 818-0545-0249', 'Krisna', true],
   // [number, dia creator/owner?, dia developer?]
@@ -89,10 +89,10 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6281268416245'
-global.nomorown = '17608914335'
+global.nomorbot = '6287859071938'
+global.nomorown = '6282136312583'
 global.namebot = 'Sad-Bot'
-global.nameown = 'Papah-Chan'
+global.nameown = 'VAS BAYU'
 
 /*=========== FAKE SIZE ===========*/
 global.fsizedoc = '99999999999999' // default 10TB
